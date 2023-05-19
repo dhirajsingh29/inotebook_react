0964c82d505bbacdb17835d5b00d12a865ec1513
dhiraj_slt <dhiraj.kumar@sevenlakes.com>
2023-05-19 14:28:36 +0530

    Initial Mongo Connection Setup

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://127.0.0.1:27017/';
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI)
+    .then(
+        () => {console.log('Connected to Mongo successfully');},
+        err => { console.error(err);}
+    );
+}
+
+module.exports = connectToMongo;
